dbfaace2ee6c53d8acb6915c67779654cafa4200
advancingKoala <milaiduoduo4567@163.com>
2018-10-30 10:40:20 +0800

    为加快响应速度，暂时把登录相关axios请求改为本地模拟数据处理，不发请求。

--- front/src/store/modules/user.js
+++ front/src/store/modules/user.js
@@ -1,5 +1,13 @@
-import { login, logout, getInfo } from '@/api/login'
-import { getToken, setToken, removeToken } from '@/utils/auth'
+import {
+  // login,
+  logout,
+  getInfo
+} from '@/api/login'
+import {
+  getToken,
+  setToken,
+  removeToken
+} from '@/utils/auth'
 
 const user = {
   state: {
@@ -26,55 +34,125 @@ const user = {
 
   actions: {
     // 登录
-    Login({ commit }, userInfo) {
-      const username = userInfo.username.trim()
+    Login({
+      commit
+    }, userInfo) {
+      // const username = userInfo.username.trim()
       return new Promise((resolve, reject) => {
-        login(username, userInfo.password).then(response => {
-          const data = response.data
-          setToken(data.token)
-          commit('SET_TOKEN', data.token)
+        /*暂时改为本地模拟数据，不发请求 */
+        try {
+          const mockResponse = {
+            code: 20000,
+            data: {
+              token: 'admin'
+            }
+          }
+          setToken(mockResponse.data.token)
+          commit('SET_TOKEN', mockResponse.data.token)
           resolve()
-        }).catch(error => {
-          reject(error)
-        })
+        } catch (e) {
+          reject(e)
+        }
+        /*暂时改为本地模拟数据，不发请求 */
+
+        /*注释原发送请求代码 */
+        // login(username, userInfo.password).then(response => {
+        //   console.log("login response:", response);
+        //   const data = response.data
+        //   setToken(data.token)
+        //   commit('SET_TOKEN', data.token)
+        //   resolve()
+        // }).catch(error => {
+        //   reject(error)
+        // })
+        /*注释原发送请求代码 */
       })
     },
 
     // 获取用户信息
-    GetInfo({ commit, state }) {
+    GetInfo({
+      commit,
+      state
+    }) {
       return new Promise((resolve, reject) => {
-        getInfo(state.token).then(response => {
-          const data = response.data
-          if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
-            commit('SET_ROLES', data.roles)
+        /*暂时改为本地模拟数据，不发请求 */
+        try {
+          const userInfoResponse = {
+            code: 20000,
+            data: {
+              name: "admin",
+              avatar: "",
+              roles: ["admin"]
+            }
+          };
+          console.log("userInfoResponse.data.roles:", userInfoResponse.data.roles);
+          if (userInfoResponse.data.roles && userInfoResponse.data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
+            commit('SET_ROLES', userInfoResponse.data.roles)
           } else {
             reject('getInfo: roles must be a non-null array !')
           }
-          commit('SET_NAME', data.name)
-          commit('SET_AVATAR', data.avatar)
-          resolve(response)
-        }).catch(error => {
-          reject(error)
-        })
+          commit('SET_NAME', userInfoResponse.data.name)
+          commit('SET_AVATAR', userInfoResponse.data.avatar)
+          resolve(userInfoResponse)
+        } catch (e) {
+          console.log("getUserInfo Error:", e);
+          reject(e)
+        }
+        /*暂时改为本地模拟数据，不发请求 */
+
+        /*注释原发送请求代码 */
+        // getInfo(state.token).then(response => {
+        //   console.log("user info response:", response);
+        //   const data = response.data
+        //   if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
+        //     commit('SET_ROLES', data.roles)
+        //   } else {
+        //     reject('getInfo: roles must be a non-null array !')
+        //   }
+        //   commit('SET_NAME', data.name)
+        //   commit('SET_AVATAR', data.avatar)
+        //   resolve(response)
+        // }).catch(error => {
+        //   reject(error)
+        // })
+        /*注释原发送请求代码 */
       })
     },
 
     // 登出
-    LogOut({ commit, state }) {
+    LogOut({
+      commit,
+      state
+    }) {
       return new Promise((resolve, reject) => {
-        logout(state.token).then(() => {
+        /*暂时改为本地模拟数据，不发请求 */
+        try {
           commit('SET_TOKEN', '')
           commit('SET_ROLES', [])
           removeToken()
           resolve()
-        }).catch(error => {
-          reject(error)
-        })
+        } catch (e) {
+          reject(e)
+        }
+        /*暂时改为本地模拟数据，不发请求 */
+
+        /*注释原发送请求代码 */
+        // logout(state.token).then(() => {
+        //   commit('SET_TOKEN', '')
+        //   commit('SET_ROLES', [])
+        //   removeToken()
+        //   resolve()
+        // }).catch(error => {
+        //   reject(error)
+        // })
+        /*注释原发送请求代码 */
       })
     },
 
     // 前端 登出
-    FedLogOut({ commit }) {
+    FedLogOut({
+      commit
+    }) {
       return new Promise(resolve => {
         commit('SET_TOKEN', '')
         removeToken()
