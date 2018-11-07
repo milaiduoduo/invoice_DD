a93484f96b918f8f0bb8e9aeba9a20d55d07c38e
advancingKoala <milaiduoduo4567@163.com>
2018-11-07 21:53:23 +0800

    注释代码引起跨域访问报错

--- front/src/utils/request.js
+++ front/src/utils/request.js
@@ -12,15 +12,15 @@ import {
 const service = axios.create({
   // baseURL: process.env.BASE_API, // api 的 base_url
   baseURL: process.env.NODE_ENV === "development" ? '/api' : "", //,
-  timeout: 5000 // 请求超时时间
+  timeout: 300000 // 请求超时时间
 })
 
 // request拦截器
 service.interceptors.request.use(
   config => {
-    if (store.getters.token) {
-      config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
-    }
+    // if (store.getters.token) {
+    //   config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    // }
     return config
   },
   error => {
@@ -41,7 +41,7 @@ service.interceptors.response.use(
       Message({
         message: res.message,
         type: 'error',
-        duration: 5 * 1000
+        duration: 300 * 1000
       })
 
       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
@@ -69,7 +69,7 @@ service.interceptors.response.use(
     Message({
       message: error.message,
       type: 'error',
-      duration: 5 * 1000
+      duration: 300 * 1000
     })
     return Promise.reject(error)
   }
