7dfa45d2233bebd4fb2095db41d25fb0e61e9541
advancingKoala <milaiduoduo4567@163.com>
2018-11-30 14:10:50 +0800

    去掉http response函数中对code!=0的类似判断，不再拦截做统一处理。

--- front/src/utils/request.js
+++ front/src/utils/request.js
@@ -49,31 +49,31 @@ aioxs_ins.interceptors.response.use(
     // console.log("axios拦截器response开始！");
     const res = response.data;
 
-    if (res.code !== 0 && res.code !== "0" && res.Code !== "0" && res.Code !== 0) {
-      // console.log("axios拦截器response返回 res.code !== 0：", res);
-      Message({
-        message: res.message,
-        showClose: true,
-        type: 'error',
-        duration: 300 * 1000
-      })
+    // if (res.code !== 0 && res.code !== "0" && res.Code !== "0" && res.Code !== 0) {
+    // console.log("axios拦截器response返回 res.code !== 0：", res);
+    // Message({
+    //   message: res.message,
+    //   showClose: true,
+    //   type: 'error',
+    //   duration: 300 * 1000
+    // })
 
-      // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        MessageBox.confirm(
-          '你已被登出，可以取消继续留在该页面，或者重新登录',
-          '确定登出', {
-            confirmButtonText: '重新登录',
-            cancelButtonText: '取消',
-            type: 'warning'
-          }
-        ).then(() => {
-          // store.dispatch('FedLogOut').then(() => {
-          //   location.reload() // 为了重新实例化vue-router对象 避免bug
-          // })
-        })
-      }
-      return Promise.reject('error')
+    // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
+    if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      MessageBox.confirm(
+        '你已被登出，可以取消继续留在该页面，或者重新登录',
+        '确定登出', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).then(() => {
+        // store.dispatch('FedLogOut').then(() => {
+        //   location.reload() // 为了重新实例化vue-router对象 避免bug
+        // })
+      })
+      // }
+      //return Promise.reject('error')
     } else {
       // console.log("axios拦截器code=0");
       //关闭loading蒙版
