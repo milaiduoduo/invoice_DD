fe61673cb5445410af9e06ba8c7cf023d197e871
advancingKoala <milaiduoduo4567@163.com>
2018-12-10 16:10:03 +0800

    增加临时公告栏，编写系统更新日志

--- front/src/views/dashboard/index.vue
+++ front/src/views/dashboard/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="dashboard-container">
-    做一些数据统计，需要数据支持。。。
+    <div>
+      <h3>临时公告栏</h3>
+      <h4>系统更新日志</h4>
+      <section>
+        <p>2018-12-09. 后台PDF上传服务增加日志追踪功能。</p>
+        <p>2018-12-10. 增加密码修改功能。</p>
+      </section>
+    </div>
+    <!-- 做一些数据统计，需要数据支持。。。 -->
     <!-- <div class="dashboard-text">name:{{ name }}</div>
     <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div> -->
   </div>
